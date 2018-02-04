843efc1c0b93ec2865aec4735692f8440a1acbea
ParadoxicalV <36139928+ParadoxicalV@users.noreply.github.com>
2018-02-04 22:58:25 +0000

    Delete util.h

--- util.h
+++
@@ -1,6 +0,0 @@
-#include <Windows.h>
-#include <iostream>
-
-VOID WriteMemory(int Address, PVOID Patch, size_t Size);
-VOID OpenConsole(PCHAR Title);
-DWORD FindFunctionByModule(PCHAR ModuleName, PCHAR Function);
